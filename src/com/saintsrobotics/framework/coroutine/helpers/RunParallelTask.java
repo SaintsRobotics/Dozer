78d42bba5b81bda1921ef943431b16fc0c0c90db
drew thompson (sr1899) <horsenkat@gmail.com>
2018-01-20 13:00:47 -0800

    Priorities and theoretical run? Sloppy

--- src/com/saintsrobotics/framework/coroutine/helpers/RunParallelTask.java
+++ src/com/saintsrobotics/framework/coroutine/helpers/RunParallelTask.java
@@ -1,7 +1,6 @@
 package com.saintsrobotics.framework.coroutine.helpers;
 
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,35 +10,40 @@ import com.saintsrobotics.framework.coroutine.Task;
 public abstract class RunParallelTask extends Task {
 	
 	private List<Task> tasks;
+	private int priority = 4; 
 
 	public RunParallelTask(Task... tasks) {
 		   this.tasks = new LinkedList<>();
 		   this.tasks.addAll(Arrays.asList(tasks));
 		
 	}
+	
+	public RunParallelTask(int priority, Task... tasks) {
+		this(tasks);
+		setTaskPriority(priority);
+	}
+	
+	public void setTaskPriority(int priority) {
+		this.priority = priority; 
+		changePriorities();
+	}
+	
+	private void changePriorities() {
+		for(Object t : this.tasks) {
+			if(t instanceof RunParallelTask) {
+				((RunParallelTask) t).setTaskPriority(priority-1);
+			}
+		}
+	}
   @Override
   protected void runTask() {
-
-	    for (Iterator<Task> taskIterator = tasks.iterator(); taskIterator.hasNext();) {
-	      Task task = taskIterator.next();
-	      if (task.iterator == null) {
-	        task.iterator = task.iterator();
-	      }
-
-	      if (task.waiter != null && task.waiter.getAsBoolean()) {
-	        if (task.iterator.hasNext()) {
-	          task.waiter = task.iterator.next();
-	        } else {
-	          taskIterator.remove();
-	        }
-	      }
-	    }
+	  for(Task t : this.tasks) {
+		  Thread aa = new Thread((Runnable) t);
+		  aa.setPriority(priority); 
+		  aa.start();
+	  }
+	  
 	  }
        
 	 
- 
-  /**
-   * Runs this method in a while loop. Must wait inside.
-   */
-  protected abstract void runContinuously();
 }
