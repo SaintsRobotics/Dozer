e5e5c9717504990a9cbd2ddb71612f6830fca894
drew thompson (sr1899) <horsenkat@gmail.com>
2018-01-20 14:04:26 -0800

    Attempt to fix while loop + saving
    While testing with unstaged main.java, RunparallelTask only will run
    once. Attempting to make while loops that still allow it to run out.

--- src/com/saintsrobotics/framework/coroutine/helpers/RunParallelTask.java
+++ src/com/saintsrobotics/framework/coroutine/helpers/RunParallelTask.java
@@ -1,49 +1,58 @@
 package com.saintsrobotics.framework.coroutine.helpers;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 
 import com.saintsrobotics.framework.coroutine.Task;
 
-public abstract class RunParallelTask extends Task {
+public class RunParallelTask extends Task {
 	
 	private List<Task> tasks;
-	private int priority = 4; 
 
 	public RunParallelTask(Task... tasks) {
+			System.out.println("Parallel task constructor");
 		   this.tasks = new LinkedList<>();
 		   this.tasks.addAll(Arrays.asList(tasks));
-		
-	}
-	
-	public RunParallelTask(int priority, Task... tasks) {
-		this(tasks);
-		setTaskPriority(priority);
-	}
-	
-	public void setTaskPriority(int priority) {
-		this.priority = priority; 
-		changePriorities();
-	}
-	
-	private void changePriorities() {
-		for(Object t : this.tasks) {
-			if(t instanceof RunParallelTask) {
-				((RunParallelTask) t).setTaskPriority(priority-1);
-			}
-		}
-	}
+	}		
+
   @Override
   protected void runTask() {
-	  for(Task t : this.tasks) {
-		  Thread aa = new Thread((Runnable) t);
-		  aa.setPriority(priority); 
-		  aa.start();
+	  System.out.println("runTask called");
+	  for (Iterator<Task> taskIterator = tasks.iterator(); taskIterator.hasNext();) {
+		
+	      Task task = taskIterator.next();
+	      System.out.println("Running through task:" + task.toString());
+	      if (task.iterator == null) {
+	    	  System.out.println(task.toString() + " null. Initalizing");
+	        task.iterator = task.iterator();
+	      }
+
+	      if (task.waiter != null && task.waiter.getAsBoolean()) {
+	        if (task.iterator.hasNext()) {
+		    	  System.out.println(task.toString() + " has next");
+	          task.waiter = task.iterator.next();
+	        } else {
+		    	  System.out.println(task.toString() + " removed");
+
+	          taskIterator.remove();
+	        }
+	      }
+	    }
+      wait.forFrame();
+
+	  boolean test = true; 
+	  for(Task t : tasks) {
+		  System.out.println("At test " + t.toString());
+		  test = test && !t.waiter.getAsBoolean();
+		  System.out.println("waiter is " + t.waiter.getAsBoolean() + " test is " + test);
 	  }
-	  
+	  System.out.println("TEST VALUE");
+	  System.out.println(test);
+	  if(test) {
+		  runTask();
 	  }
-       
-	 
+}
 }
