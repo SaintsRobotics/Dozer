9c5702afe6b3391544017c1c8abb2f6e5cdfa299
drew thompson (sr1899) <horsenkat@gmail.com>
2018-01-24 16:49:40 -0800

    Finished RunTask stuff

--- src/com/saintsrobotics/framework/coroutine/helpers/RunParallelTask.java
+++ src/com/saintsrobotics/framework/coroutine/helpers/RunParallelTask.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import com.saintsrobotics.framework.coroutine.Task;
 
-public class RunParallelTask extends Task {
+public class RunParallelTask extends RunContinuousTask {
 	
 	private List<Task> tasks;
 
@@ -19,40 +19,26 @@ public class RunParallelTask extends Task {
 	}		
 
   @Override
-  protected void runTask() {
-	  System.out.println("runTask called");
+  protected boolean runContinuously() {
 	  for (Iterator<Task> taskIterator = tasks.iterator(); taskIterator.hasNext();) {
 		
 	      Task task = taskIterator.next();
-	      System.out.println("Running through task:" + task.toString());
 	      if (task.iterator == null) {
-	    	  System.out.println(task.toString() + " null. Initalizing");
 	        task.iterator = task.iterator();
 	      }
 
 	      if (task.waiter != null && task.waiter.getAsBoolean()) {
 	        if (task.iterator.hasNext()) {
-		    	  System.out.println(task.toString() + " has next");
+//		    	  System.out.println(task.toString() + " has next");
 	          task.waiter = task.iterator.next();
 	        } else {
-		    	  System.out.println(task.toString() + " removed");
+//		    	  System.out.println(task.toString() + " removed");
 
 	          taskIterator.remove();
 	        }
 	      }
-	    }
-      wait.forFrame();
+	  }
+	  return tasks.isEmpty();
+  }
+}
 
-	  boolean test = true; 
-	  for(Task t : tasks) {
-		  System.out.println("At test " + t.toString());
-		  test = test && !t.waiter.getAsBoolean();
-		  System.out.println("waiter is " + t.waiter.getAsBoolean() + " test is " + test);
-	  }
-	  System.out.println("TEST VALUE");
-	  System.out.println(test);
-	  if(test) {
-		  runTask();
-	  }
-}
-}
--- src/com/saintsrobotics/framework/coroutine/helpers/RunSequentialTask.java
+++ src/com/saintsrobotics/framework/coroutine/helpers/RunSequentialTask.java
@@ -5,23 +5,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.saintsrobotics.framework.coroutine.Task;
-import com.sun.jmx.snmp.tasks.TaskServer;
 
 		
-public abstract class RunSequentialTask extends Task {
-	private List<Task> tasks;
+public class RunSequentialTask extends Task {
+	private Task[] tasks;
 
 
 	public RunSequentialTask(Task...tasks){
-		this.tasks = new LinkedList<>();
-	    this.tasks.addAll(Arrays.asList(tasks));
+		this.tasks = tasks;
 		
 }
   @Override
   protected void runTask() {
 	  for(Task t : this.tasks) {
-		  while(t.iterator().hasNext()) {
-			  yield(t.iterator().next());
+		  t.iterator =  t.iterator();
+		  while(t.iterator.hasNext()) {
+			  yield(t.iterator.next());
 		  }
 	  }
   }
