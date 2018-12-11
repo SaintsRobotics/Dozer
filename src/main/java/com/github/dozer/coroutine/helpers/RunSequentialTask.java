74bda1a81b7374249ad1257fdc22d0c9e0c45bb5
t2001s <tara2001s@gmail.com>
2018-12-10 16:54:35 -0800

    further fixes

--- src/main/java/com/github/dozer/coroutine/helpers/RunSequentialTask.java
+++ src/main/java/com/github/dozer/coroutine/helpers/RunSequentialTask.java
@@ -5,20 +5,27 @@ import com.github.dozer.coroutine.Task;
 		
 public class RunSequentialTask extends Task {
 	private Task[] tasks;
+	private TaskRunner sequentialRunner;
 
 
 	public RunSequentialTask(Task...tasks){
 		this.tasks = tasks;
+		sequentialRunner = new TaskRunner(this.tasks);
 		
 }
   @Override
   protected void runTask() {
+    if (sequentialRunner != null) {
+      sequentialRunner.run();
+    }
+    this.sequentialRunner.disable();
+    /**
 	  for(Task t : this.tasks) {
 		  t.iterator =  t.iterator();
 		  while(t.iterator.hasNext()) {
 			  yield(t.iterator.next());
 		  }
-	  }
+	  }**/
   }
 	public void free(){
 		for(Task task: tasks){
